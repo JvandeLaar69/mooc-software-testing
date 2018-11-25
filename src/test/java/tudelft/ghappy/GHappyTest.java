65109efa74325d371ac40b9615f2293ed1a458fe
Jurian van de Laar <jvandelaar@kns.com>
2018-11-25 23:01:29 +0100

    Commit changes After Day 2, with GHappy exercise

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,52 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+
+    @Test
+        public void singleGAtStart() {
+            GHappy gh = new GHappy();
+            boolean result = gh.gHappy("gXffc");
+            Assertions.assertFalse(result);
+        }
+
+    @Test
+        public void doubleGAtStart() {
+            GHappy gh = new GHappy();
+            boolean result = gh.gHappy("ggXffc");
+            Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void singleGAtSecondPostion() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("qgXffc");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void doubleGAtSecondPostion() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("qggXffc");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void doubleGAtMultiplePostions() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("qggXffgggc");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void singleAndMultipleGMixed() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("qgXffgggc");
+        Assertions.assertFalse(result);
+    }
+
+
 }
